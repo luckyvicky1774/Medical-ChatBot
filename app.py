652e6b4d100f612042237c6726333efb0f9ef50e
Shu Q <shuq@mac.home>
2026-03-30 00:39:09 -0400

    prepare app for render deployment

--- app.py
+++ app.py
@@ -67,4 +67,5 @@ def chat():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 8081, debug= True)
+    port = int(os.environ.get("PORT", 8081))
+    app.run(host="0.0.0.0", port=port, debug=False)
